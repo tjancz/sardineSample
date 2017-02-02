4aede7e293545d9712946c53c0ec1c572fa1933f
Tomasz TJ Janczewski <tjancz@gmail.com>
2017-02-02 14:54:08 +0100

    First working listing

--- src/main/java/pl/pio/tools/sardine/Synchro.java
+++ src/main/java/pl/pio/tools/sardine/Synchro.java
@@ -1,17 +1,47 @@
 package pl.pio.tools.sardine;
 
+import com.github.sardine.DavResource;
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by tomasz on 01.02.17.
  */
 public class Synchro {
+    private Sardine connection;
+
+
+    private Synchro(String[] args) {
+        connection = SardineFactory.begin(args[1],args[2]);
+    }
 
     public static void main(String... args) throws IOException {
-        Sardine connection = SardineFactory.begin(args[1],args[2]);
-        connection.list(args[0]).forEach(System.out::println);
+        final Synchro synchro = new Synchro(args);
+
+        Optional<DavResource> davResourceOptional = synchro.getRootResource(synchro.getConnection().list(args[0]+"/remote.php/webdav/"));
+        if(davResourceOptional.isPresent()) {
+            Optional<DavResource> davResource = synchro.getActualNo(args[0] + davResourceOptional.get().getPath());
+        }
+
+    }
+
+    private Optional<DavResource> getActualNo(String path) throws IOException {
+        connection.list(path,1,true).stream().filter(resource -> "application/pdf".equals(resource.getContentType())).forEach(resource -> System.out.println(resource.getModified()));
+        return Optional.empty();
+    }
+
+    private Sardine getConnection() {
+        return connection;
+    }
+
+    private Optional<DavResource> getRootResource(List<DavResource> list) throws IOException {
+        return list.stream().filter(devResource -> "magazyn_programista".equals(devResource.getName())).findFirst();
     }
 }
