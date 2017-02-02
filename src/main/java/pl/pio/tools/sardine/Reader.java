e2834f7156bb0e39cf9b7fa0f0a1e8a6eb6aa562
Tomasz TJ Janczewski <tjancz@gmail.com>
2017-02-02 16:26:40 +0100

    Refactoring

--- src/main/java/pl/pio/tools/sardine/Reader.java
+++ src/main/java/pl/pio/tools/sardine/Reader.java
@@ -4,38 +4,35 @@ import com.github.sardine.DavResource;
 import com.github.sardine.Sardine;
 import com.github.sardine.SardineFactory;
 import org.apache.commons.io.FileUtils;
-import sun.misc.IOUtils;
 
-import java.awt.*;
+import java.awt.Desktop;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
 import java.util.List;
 import java.util.Optional;
 
 /**
  * Created by tomasz on 01.02.17.
  */
-public class Synchro {
+public class Reader {
     private Sardine connection;
 
 
-    private Synchro(String[] args) {
+    private Reader(String[] args) {
         connection = SardineFactory.begin(args[1],args[2]);
     }
 
     public static void main(String... args) throws IOException {
-        final Synchro synchro = new Synchro(args);
+        final Reader reader = new Reader(args);
 
-        Optional<DavResource> davResourceOptional = synchro.getRootResource(synchro.getConnection().list(args[0]+"/remote.php/webdav/"));
+        Optional<DavResource> davResourceOptional = reader.getRootResource(reader.getConnection().list(args[0]+"/remote.php/webdav/"));
         if(davResourceOptional.isPresent()) {
-            Optional<DavResource> davResource = synchro.getActualNo(args[0] + davResourceOptional.get().getPath());
+            Optional<DavResource> davResource = reader.getActualNo(args[0] + davResourceOptional.get().getPath());
             if(davResource.isPresent()) {
                 final DavResource magazyn = davResource.get();
                 final File temporary = File.createTempFile("magazyn",".pdf");
-                final InputStream magazynStream = synchro.getConnection().get(args[0]+magazyn.getPath());
+                final InputStream magazynStream = reader.getConnection().get(args[0]+magazyn.getPath());
                 FileUtils.copyInputStreamToFile(magazynStream,temporary);
                 Desktop.getDesktop().open(temporary);
             }
